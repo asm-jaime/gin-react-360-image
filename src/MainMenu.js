995f57af8358c60c5343dda8a128dcd4e2c4a5df
Jaime <asm.jaime@gmail.com>
2020-02-12 11:15:58 +1000

    component/MainMenu/add tip elements

--- src/MainMenu.js
+++ src/MainMenu.js
@@ -1,10 +1,24 @@
 import React  from 'react';
+import PropTypes from 'prop-types';
+
 import './MainMenu.css';
+
+const MenuElement = (props) => {
+  return (
+    <div className='menu-element'>{props.children}</div>
+  );
+}
+
+MenuElement.propTypes = {
+  children: PropTypes.element.isRequired
+};
 
 const MainMenu = () => {
   return (
     <div className='main-menu'>
-      something
+      <MenuElement>click on an item in the item list</MenuElement>
+      <MenuElement>mouse down on the pic and move</MenuElement>
+      <MenuElement>mouse down on the pic and scroll</MenuElement>
     </div>
   );
 }
