536698f382660e32fa0c51fc0b63bcfbe0d6fcfc
Jaime <asm.jaime@gmail.com>
2020-02-13 15:26:15 +1000

    component/Item360/del/prev state for indexes

--- src/Item360.js
+++ src/Item360.js
@@ -7,29 +7,27 @@ import { PIXELS_PER_DEG } from './Constants';
 const Item360 = (props) => {
   const [drag, setDrag] = React.useState({start: 0, startIndex: 0});
   const [dragging, setDragging] = React.useState(false);
-  const [imageIndex, setImageIndex] = React.useState({now: 0, prev: 0});
-  const [quality, setQuality] = React.useState({now: 0, prev: 0});
+  const [imageIndex, setImageIndex] = React.useState(0);
+  const [quality, setQuality] = React.useState(0);
 
   const [image, setImage] = React.useState('');
 
   const handleMouseWheel = e => {
     e.preventDefault();
 
-    if(e.deltaY < 0 && quality.now < props.item.qualitysize - 1) {
-      setQuality({ now: quality.now + 1, prev: quality.now });
-      setImageIndex({ now: imageIndex.now, prev: imageIndex.now });
-      props.getImage(imageIndex.now, quality.now).then(setImage);
-    } else if (e.deltaY > 0 && quality.now > 0) {
-      setQuality({ now: quality.now - 1, prev: quality.now });
-      setImageIndex({ now: imageIndex.now, prev: imageIndex.now });
-      props.getImage(imageIndex.now, quality.now).then(setImage);
+    if(e.deltaY < 0 && quality < props.item.qualitysize - 1) {
+      setQuality(quality + 1);
+      props.getImage(imageIndex, quality).then(setImage);
+    } else if (e.deltaY > 0 && quality > 0) {
+      setQuality(quality - 1);
+      props.getImage(imageIndex, quality).then(setImage);
     }
   };
 
   const handleMouseDown = e => {
     e.persist();
     setDragging(true);
-    setDrag({start: e.screenX, startIndex: imageIndex.now});
+    setDrag({start: e.screenX, startIndex: imageIndex});
   };
 
   const handleMouseUp = () => {
@@ -49,9 +47,9 @@ const Item360 = (props) => {
     }
     index = (index + drag.startIndex) % numImages;
 
-    if (index !== imageIndex.now) {
-      setImageIndex({now: index, prev: imageIndex.now});
-      props.getImage(imageIndex.now, quality.now).then(setImage);
+    if (index !== imageIndex) {
+      setImageIndex(index);
+      props.getImage(imageIndex, quality).then(setImage);
     }
   };
 
@@ -65,7 +63,7 @@ const Item360 = (props) => {
     document.addEventListener('mousemove', handleMouseMove, false);
     document.addEventListener('mouseup', handleMouseUp, false);
     document.getElementById('item360').addEventListener('wheel', handleMouseWheel, false);
-    props.getImage(imageIndex.now, quality.now).then(setImage);
+    props.getImage(imageIndex, quality).then(setImage);
     return () => {
       document.getElementById('item360')
         .removeEventListener('wheel', handleMouseWheel, false);
@@ -80,14 +78,14 @@ const Item360 = (props) => {
 
   return (
     <div className='item360'>
-    <div className='item360-quality'>quality: {quality.now}</div>
-    <div className='image-container'
-      onMouseDown={handleMouseDown}
-      onDragStart={preventDragHandler}
-      id='item360'
-    >
-    <img className='image-360' alt='' src={image} />
-    </div>
+      <div className='item360-quality'>quality: {quality}</div>
+      <div className='image-container'
+        onMouseDown={handleMouseDown}
+        onDragStart={preventDragHandler}
+        id='item360'
+      >
+        <img className='image-360' alt='' src={image} />
+      </div>
     </div>
   );
 }
