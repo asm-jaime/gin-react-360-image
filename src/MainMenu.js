f270455ae8b5acd79b2afffc27b09cac72d37b97
Jaime <asm.jaime@gmail.com>
2020-02-16 16:39:25 +1000

    data/add mokey-360 dataset

--- src/MainMenu.js
+++ src/MainMenu.js
@@ -2,9 +2,19 @@ import React  from 'react';
 
 import './MainMenu.css';
 
+import { Store } from './Store';
+
 const MainMenu = () => {
+  const { state } = React.useContext(Store);
   return (
     <div className='main-menu'>
+      <button onClick={()=>{
+        const myjson = JSON.stringify(state, null, 2);
+        const x = window.open();
+        x.document.open();
+        x.document.write('<html><body><pre>' + myjson + '</pre></body></html>');
+        x.document.close();
+      }}>open</button>
       <div className='menu-element'>click on an item in the item list</div>
       <div className='menu-element'>mouse down on the pic and move</div>
       <div className='menu-element'>mouse down on the pic and scroll</div>
